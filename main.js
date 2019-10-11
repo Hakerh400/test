cb88906ed722c7f7388595a794bbadd1561acf88
Thomas <hakerh403@gmail.com>
2019-10-11 21:34:19 +0200

    commit

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 'use strict';
-
-const defaultBrowser =
-  require('child_process').execSync(`
-    reg query HKEY_CURRENT_USER\\Software\\Classes\\http\\shell\\open\\command
-  /ve`.replace(/\s/g, ' ')).toString().trim().split(/\t|\s{2,}/).pop().
-  match(/".*?"|\S+/)[0].replace(/"/g, '');
-
-console.log(defaultBrowser);
